29a1c7b241e9911da0a49eae2ce5eb2a5107c100
Rafael Wayar <rafael.wt@gmail.com>
2018-04-29 13:20:47 -0400

    finalizada seccion 9 leccion 148 v2

--- src/app/components/precios/precios.component.ts
+++ src/app/components/precios/precios.component.ts
@@ -1,22 +1,15 @@
 import { Component, OnInit } from '@angular/core';
-import { AuthService } from '../../services/auth.service';
+
 @Component({
   selector: 'app-precios',
   templateUrl: './precios.component.html',
   styles: []
 })
 export class PreciosComponent implements OnInit {
-  profile: any;
-  constructor(public auth: AuthService) { }
+  constructor() { }
 
   ngOnInit() {
-    if (this.auth.userProfile) {
-      this.profile = this.auth.userProfile;
-    } else {
-      this.auth.getProfile((err, profile) => {
-        this.profile = profile;
-      });
-    }
+
   }
 
 }
